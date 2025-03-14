dd0d437fe683c8c11a2be52a3e30c8a3b43c3495
Willz <Obiadichinwuba@yahoo.com>
2025-03-14 12:09:01 +0100

    Add files via upload

---
+++ foodieland/JS/script.js
@@ -0,0 +1,46 @@
+let open = document.querySelector('.bx-menu');
+let close = document.querySelector('.bx-x');
+let nav = document.querySelector('.nav-links');
+
+open.addEventListener("click", () => {
+    nav.classList.toggle('active');
+
+})
+
+close.addEventListener("click", () => {
+    nav.classList.toggle('active');
+    
+}) 
+
+
+// Mobile navigation Bar Ends at this Point
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function HamburgerOpen(click) {
+//     if (1 > 5) {
+//         'I love God '
+//     } else {
+//         'I will walk with God'
+//     }
+
+// }
